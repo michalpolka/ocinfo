d05537e00e0b99120bc59555fd20c9be6011e5fc
Michal Polka <michal@michalpolka.me>
2024-03-22 14:23:58 +0100

    Modified canvas js

--- exercices/m09/canvas-global.js
+++ exercices/m09/canvas-global.js
@@ -1,3 +1,9 @@
 function $(id) {
     return document.getElementById(id);
 }
+
+function initCanvas(id) {
+    ctx = $(id).getContext('2d');
+    width = parseInt($(id).width);
+    height = parseInt($(id).height)
+}
